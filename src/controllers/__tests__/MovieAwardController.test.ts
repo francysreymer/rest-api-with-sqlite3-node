481e685fa0a14b109e707ab402b3de0e6798630c
Francys <francys.reymer@gmail.com>
2024-11-19 18:18:19 -0400

    add complement tests

--- src/controllers/__tests__/MovieAwardController.test.ts
+++ src/controllers/__tests__/MovieAwardController.test.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { writeFileSync, unlinkSync, existsSync } from 'fs';
+import { writeFileSync, unlinkSync, existsSync, readFileSync } from 'fs';
+import { StatusCodes } from 'http-status-codes';
 import path from 'path';
 import request from 'supertest';
 
@@ -12,8 +13,17 @@ import { WinnerProducer } from '@/types/WinnerProducer';
 
 const URL_API = '/api/movies/producers/award-intervals';
 const tempFilePath = path.join(__dirname, 'testData.csv');
+const standardFilePath = path.join(
+  __dirname,
+  'testData',
+  '../../../../temp/movielist.csv',
+);
+
 const writeCSVData = (data: string) => {
   writeFileSync(tempFilePath, data);
+};
+const readCSVData = () => {
+  return readFileSync(standardFilePath, 'utf-8');
 };
 
 // Set up the Express app
@@ -40,7 +50,7 @@ describe('MovieAwardController', () => {
     await migrationService.revertMigrations();
   });
 
-  it('should return producers with the maximum award intervals', async () => {
+  it('should return producers with the MAX award intervals (using fictitious data)', async () => {
     // Create a temporary CSV file with sample data
     writeCSVData(
       'year;title;studios;producers;winner\n2000;First Win;Studio A;Producer A;yes\n2010;Second Win;Studio B;Producer A;yes\n2011;Single Win;Studio C;Producer B;yes\n',
@@ -63,11 +73,11 @@ describe('MovieAwardController', () => {
       },
     ];
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(StatusCodes.OK);
     expect(response.body.max).toEqual(expectedMaxWinners);
   });
 
-  it('should return producers with the minimum award intervals', async () => {
+  it('should return producers with the MIN award intervals (using fictitious data)', async () => {
     // Create a temporary CSV file with different sample data
     writeCSVData(
       'year;title;studios;producers;winner\n2000;First Win;Studio A;Producer A;yes\n2011;Single Win;Studio C;Producer B;yes\n2012;Second Win;Studio B;Producer A;yes\n2013;Single Win 2;Studio E;Producer B;yes\n',
@@ -90,11 +100,11 @@ describe('MovieAwardController', () => {
       },
     ];
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(StatusCodes.OK);
     expect(response.body.min).toEqual(expectedMinWinners);
   });
 
-  it('should handle multiple producers with the same max and min intervals', async () => {
+  it('should handle multiple producers with the same MAX and MIN intervals (using fictitious data)', async () => {
     // Create a temporary CSV file with different sample data
     writeCSVData(
       'year;title;studios;producers;winner\n2012;First Win Test;Studio A;Producer B;yes\n2000;First Win;Studio A;Producer A;yes\n2010;Second Win;Studio B;Producer A;yes\n2011;Single Win;Studio C;Producer B;yes\n2000;Another First MAX Win;Studio D;Producer D;yes\n2010;Another Second MAX Win;Studio E;Producer D;yes\n2000;Another First MIN Win;Studio F;Producer E;yes\n2001;Another Second MIN Win;Studio G;Producer E;yes\n',
@@ -138,12 +148,12 @@ describe('MovieAwardController', () => {
       },
     ];
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(StatusCodes.OK);
     expect(response.body.max).toEqual(expectedMaxWinners);
     expect(response.body.min).toEqual(expectedMinWinners);
   });
 
-  it('should not return intervals of zero', async () => {
+  it('should not return intervals of zero (using fictitious data)', async () => {
     // Create a temporary CSV file with different sample data
     writeCSVData(
       'year;title;studios;producers;winner\n2011;Zero Interval Win 1;Studio A;Producer A;yes\n2011;Zero Interval Win 2;Studio B;Producer A;yes\n',
@@ -165,7 +175,7 @@ describe('MovieAwardController', () => {
     expect(response.body.max).toEqual(expectedMaxWinners);
   });
 
-  it('should return empty arrays when there are no winners', async () => {
+  it('should return empty arrays when there are no winners (using fictitious data)', async () => {
     // Create a temporary CSV file with different sample data
     writeCSVData(
       'year;title;studios;producers;winner\n2011;No Winners 1;Studio A;Producer A;no\n2011;No Winners 2;Studio B;Producer A;no\n',
@@ -182,8 +192,81 @@ describe('MovieAwardController', () => {
     const expectedMinWinners: WinnerProducer[] = [];
     const expectedMaxWinners: WinnerProducer[] = [];
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(StatusCodes.OK);
     expect(response.body.min).toEqual(expectedMinWinners);
     expect(response.body.max).toEqual(expectedMaxWinners);
+  });
+
+  it('should return producers with the MAX and MIN award intervals (using standard CSV data)', async () => {
+    // Read the standard CSV data
+    const standardData = readCSVData();
+    writeCSVData(standardData);
+
+    // Seed the database
+    const seedService = container.get<SeedFromCSVService>(
+      TYPES.SeedFromCSVService,
+    );
+    await seedService.initializeDataFrom(tempFilePath);
+
+    const expectedMaxWinners: WinnerProducer[] = [
+      {
+        producer: 'Matthew Vaughn',
+        interval: 13,
+        previousWin: 2002,
+        followingWin: 2015,
+      },
+    ];
+
+    const expectedMinWinners: WinnerProducer[] = [
+      {
+        producer: 'Joel Silver',
+        interval: 1,
+        previousWin: 1990,
+        followingWin: 1991,
+      },
+    ];
+
+    const response = await request(app).get(URL_API);
+
+    expect(response.status).toBe(StatusCodes.OK);
+    expect(response.body.max).toEqual(expectedMaxWinners);
+    expect(response.body.min).toEqual(expectedMinWinners);
+  });
+
+  it('should fail if the data does not match the expected MAX and MIN award intervals (using standard CSV data)', async () => {
+    // Create a temporary CSV file with different sample data
+    writeCSVData(
+      'year;title;studios;producers;winner\n2011;Zero Interval Win 1;Studio A;Producer A;yes\n2011;Zero Interval Win 2;Studio B;Producer A;yes\n',
+    );
+
+    // Seed the database
+    const seedService = container.get<SeedFromCSVService>(
+      TYPES.SeedFromCSVService,
+    );
+    await seedService.initializeDataFrom(tempFilePath);
+
+    const expectedMaxWinners: WinnerProducer[] = [
+      {
+        producer: 'Matthew Vaughn',
+        interval: 13,
+        previousWin: 2002,
+        followingWin: 2015,
+      },
+    ];
+
+    const expectedMinWinners: WinnerProducer[] = [
+      {
+        producer: 'Joel Silver',
+        interval: 1,
+        previousWin: 1990,
+        followingWin: 1991,
+      },
+    ];
+
+    const response = await request(app).get(URL_API);
+
+    expect(response.status).toBe(StatusCodes.OK);
+    expect(response.body.min).not.toEqual(expectedMinWinners);
+    expect(response.body.max).not.toEqual(expectedMaxWinners);
   });
 });
